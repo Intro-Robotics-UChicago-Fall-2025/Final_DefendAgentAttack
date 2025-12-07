6b17d1f3f0b9c1fa5e43ea36c7d739edc1eba952
hanimn12 <hanimn@uchicago.edu>
2025-12-07 16:06:13 -0600

    finding actions done

--- defendagentattack/matricies/state_nstate_generate.py
+++ defendagentattack/matricies/state_nstate_generate.py
@@ -1,18 +1,65 @@
 import numpy as np
-from ament_index_python.packages import get_package_share_directory
-
+import math 
 import os
+
+
+
+def helper_valid_act(n_st, c_st, act_ls,
+                      who): # if defense is moving bod y, arm has to be zero 
+    state_to_act = {0: [0,1,2], 1: [0,3,4]}
+
+    if who == "A" and  n_st[1] > 0 and n_st[0] == 0: 
+#for attack, no action allowed to achieve a state where teh arm is not at home base adn the body is home, harms robot
+        return None 
+    else: 
+        
+
+        n_st =np.array(n_st)
+        c_st = np.array(c_st)
+
+        diff = n_st - c_st # get the difference for each index 
+
+         # check if you have moved anything  -- no restrictions here 
+        tot_mov = sum(abs(diff))
+
+        if tot_mov == 0:  # maybe change this where they can use the same kaction 
+
+
+            return None 
+        
+        if 0 in diff: # if at least one part of the body is not moving 
+            idx_ch =  int(np.where(diff!= 0)[0])# index for where the change is            
+            change_st = int(n_st[idx_ch]) # get the state of the body arf of interest 
+
+
+
+# state-to-act[idx_ch] gets teh list of possible actions based on that body part 
+# getting change_st idx will allow you to map to the action list of the player 
+            act_val = state_to_act[idx_ch][change_st]
+        
+
+
+            return act_val
+    
+        
+        else: 
+            return None
+
 
 def main():
 
     # Fetch Actions and states (saved files)
-    share = get_package_share_directory('defendagentattack')
+    matrix_pah = "."
 
-    action_pth = os.path.join(share, 'matrices', 'action.txt')
+    action_pth = os.path.join(matrix_pah, "actions.txt")
+
     actions = np.loadtxt(action_pth)
 
-    state_pth = os.path.join(share, 'matrices', 'states.txt')
+    actions = np.loadtxt(action_pth)
+
+    state_pth = os.path.join(matrix_pah, "states.txt")
     states = np.loadtxt(state_pth)
+    print(states)
 
     s_len = len(states)
     
@@ -22,8 +69,6 @@ def main():
     # next state is only possible if both robots perform an action ---> can't stay still
     # with what states are actions avaialbe
 
-    curr_s = states[0]
-
 
         # self.player_exp_actions = {"attack": ["home","M_R","M_L","arm"],
         #                       "defend" : ["home", "A_R", "A_L"]}
@@ -31,77 +76,37 @@ def main():
     for curr_idx, next_list in enumerate(state_nstate):# for every row index (represents a state )
         # look at each column item ===> get current 
         curr_state = np.array(states[curr_idx])
-        for next_idx, val in enumerate(next_list):
+        for next_idx, val in enumerate(next_list): # for every next_state get the index of stata, and the value ---> set the value at the end of the function 
 
                     # self.player_exp_actions = {"attack": ["home","M_R","M_L","arm"],
                     #           "defend" : ["home", "A_R", "A_L"]}
             # 
-            n_state = np.array(states[next_idx])
+            n_state = np.array(states[next_idx]) # get the next state 
 
-            diff = n_state - curr_state # calculate the diff [0000]--> 1010 (move_r - A, turn  - D )
-            # cases when it doesnt work  0000 -> 
-
-            if sum(diff) <= 1: # both agents need to perform an action
-                continue
-
-            a_n_state = n_state[0:1]
-            a_cur_state = curr_state[0:1]
-            d_n_state = n_state[2:3] 
-            d_curr_state = curr_state[2:3]
+            a_n_st= n_state[0:2]
+            a_cur_st= curr_state[0:2]
+            d_n_st = n_state[2:4]
+            d_curr_s = curr_state[2:4]
 
             # get the position of the person 
 
-            # attack [0,1] --> im facing opnening, and arm donw in direction 
-            # attack index 0 has to be > 0 for index 1 to be greater than 0.
-            a_arm_bool = a_cur_state[0] == a_n_state[0] and a_cur_state[0] > 0 
-            # will be true if you are allowed to move arm down. 
-            
+            a_act = helper_valid_act(a_n_st, a_cur_st, actions, "A")          
+            d_act = helper_valid_act(d_n_st, d_curr_s, actions, "D")
 
-            a_diff = a_n_state - a_cur_state
-            d_diff = d_n_state - d_curr_state 
-
-            # what ws it before, what is it next ==> what is the action that gets you there
-
-          #  0000 --> 1010 Attack mmove R, Deffend arm_back right --> action [1,1]
-          # 1010 --> 1111 Attack arm down  R, move arm 
+            # if eitheer of the acts are none --> continue 
+            if a_act == None or d_act == None: 
+                continue 
 
 
+            new_act = [a_act, d_act]
+            find_act = [i for i, state in enumerate(actions) if state.tolist() == new_act][0]
+            if find_act != 0: 
+                find_act += 1 
 
-        # Atack agent states are indicies 0-1 and Defend are 2-3 
-        # index 0 represents the Attack Agent stance relative to Defense
-        # ==> 0 - in front, 1 - to the Right, 2 - to the left 
-        # index 1 represens the A Agent arm up (0), or down (1)
-        # index 2 represents the D Agent stance relative to Defense
-        # ===> forward facing (0), not seeing (1)
-        # index 3 represents the D Agent Arm position 
-        # ==> home/up (0), arm_down_right (1), arm_down_left (2)
-            
+            state_nstate[curr_idx][next_idx] = int(find_act) # or the defalt value 
 
-            # what actions get you to the best states 
-
-         # Fetch Actions and states (saved files)
-         # Joint action --> [Attack, Defender]
-         # Attack Agent can Face/Turn to face opponent (0). Move_R/L (1,2), arm down in direction of move (3)
-         # Defend Agent can Turn to Face (0), arm_back_right/left (1,2) 
-
-
-            # check if both robots actually took an action
-
-
-
-
-
-
-
-
-
-
-
-    None 
-    # do something here to generate the data
-
-
-
+    file_name = "state_nstate.txt"
+    np.savetxt(file_name, state_nstate.astype(int), fmt='%d')
 
 if __name__ == "__main__":
-    main()
+    main() 
